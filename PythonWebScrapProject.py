08ea4819dd650f129b7f8670aeeb7776ebab796e
Fooxie <fabdamiao@outlook.com>
2024-09-17 21:30:11 -0300

    .atualizacao pra corrigir as horas .atualizacao da direcao de vento .atualizacao de nomenclatura

--- PythonWebScrapProject.py
+++ PythonWebScrapProject.py
@@ -1,10 +1,15 @@
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+import os
+from datetime import datetime, timedelta
+import math
 
 # Configurações do Selenium
 chrome_options = Options()
@@ -12,8 +17,10 @@ chrome_options.add_argument("--headless")
 chrome_options.binary_location = "C:\\Users\\fabda\\Documents\\chrome-win64\\chrome.exe"
 chrome_service = Service('chromedriver.exe')
 
+
 def get_driver():
     return webdriver.Chrome(service=chrome_service, options=chrome_options)
+
 
 driver = get_driver()
 
@@ -64,25 +71,17 @@ postos = {
 }
 
 
-
-from bs4 import BeautifulSoup
-from datetime import datetime
-import time
-
 def get_station_data(station_url, retries=3, page_timeout=30):
     global driver
     for attempt in range(retries):
         try:
             time.sleep(8)
-
             driver.set_page_load_timeout(page_timeout)
             driver.get(station_url)
-
 
             WebDriverWait(driver, page_timeout).until(
                 lambda d: d.execute_script('return document.readyState') == 'complete'
             )
-
 
             WebDriverWait(driver, 10).until(
                 EC.presence_of_element_located((By.TAG_NAME, 'table'))
@@ -97,14 +96,13 @@ def get_station_data(station_url, retries=3, page_timeout=30):
 
                 # Extraindo os dados da tabela
                 data = {
-                    "Chuva(mm)": "",
-                    "Temp(oC)": "",
-                    "Umid.Rel.(%)": "",
-                    "Vel.VT(m/s)": "",
-                    "Dir.VT(o)": "",
-                    "Pressão(mb)": "",
-                    "Data": datetime.now().strftime('%Y-%m-%d'),
-                    "Hora": datetime.now().strftime('%H:00')
+                    "qtd_chuva": "",
+                    "nr_temp": "",
+                    "nr_umid": "",
+                    "nr_vel_vt": "",
+                    "nr_dir_vt": "",
+                    "nr_pressao": "",
+                    "data_hora": datetime.now().strftime('%d-%m-%y %H:00')
                 }
 
                 for row in rows:
@@ -112,17 +110,17 @@ def get_station_data(station_url, retries=3, page_timeout=30):
                     if len(columns) == 2:
                         key, value = columns[0].text.strip(), columns[1].text.strip()
                         if key.startswith("Per. Atual"):
-                            data["Chuva(mm)"] = value.split()[0]
+                            data["qtd_chuva"] = value.split()[0]
                         elif key.startswith("Atual:") and "ºC" in value:
-                            data["Temp(oC)"] = value.split()[0]
+                            data["nr_temp"] = value.split()[0]
                         elif key.startswith("Atual:") and "%" in value:
-                            data["Umid.Rel.(%)"] = value.split()[0]
+                            data["nr_umid"] = value.split()[0]
                         elif key.startswith("Direção:"):
-                            data["Dir.VT(o)"] = value
+                            data["nr_dir_vt"] = value
                         elif key.startswith("Velocidade:"):
-                            data["Vel.VT(m/s)"] = f"{float(value.split()[0].replace('km/h', '')) * 1000 / 3600:.2f}"
+                            data["nr_vel_vt"] = f"{float(value.split()[0].replace('km/h', '')) * 1000 / 3600:.2f}"
                         elif key.startswith("Atual:") and "hPa" in value:
-                            data["Pressão(mb)"] = value.split()[0]
+                            data["nr_pressao"] = value.split()[0]
 
                 print(f"Tabela encontrada em {station_url}. Dados extraídos.")
                 return data
@@ -134,16 +132,14 @@ def get_station_data(station_url, retries=3, page_timeout=30):
         except Exception as e:
             print(f"Erro ao carregar os dados: {e}")
             if attempt == retries - 1:
-
                 data = {
-                    "Chuva(mm)": "",
-                    "Temp(oC)": "",
-                    "Umid.Rel.(%)": "",
-                    "Vel.VT(m/s)": "",
-                    "Dir.VT(o)": "",
-                    "Pressão(mb)": "",
-                    "Data": datetime.now().strftime('%Y-%m-%d'),
-                    "Hora": datetime.now().strftime('%H:00')
+                    "qtd_chuva": "",
+                    "nr_temp": "",
+                    "nr_umid": "",
+                    "nr_vel_vt": "",
+                    "nr_dir_vt": "",
+                    "nr_pressao": "",
+                    "data_hora": datetime.now().strftime('%d-%m-%y %H:00')
                 }
                 return data
             else:
@@ -154,12 +150,37 @@ def get_station_data(station_url, retries=3, page_timeout=30):
     return None
 
 
+def wind_direction_to_degrees(direction):
+    wind_directions = {
+        'N': 0.0, 'NNE': 22.5, 'NE': 45.0, 'ENE': 67.5,
+        'E': 90.0, 'ESE': 112.5, 'SE': 135.0, 'SSE': 157.5,
+        'S': 180.0, 'SSW': 202.5, 'SW': 225.0, 'WSW': 247.5,
+        'W': 270.0, 'WNW': 292.5, 'NW': 315.0, 'NNW': 337.5,
+        '': None  # Para valores em branco, retornamos None
+    }
 
-import pandas as pd
-import os
+    # Converter para maiúsculas e remover espaços extras
+    direction = direction.strip().upper()
+
+    # Retornar o grau correspondente ou None se não encontrado
+    return wind_directions.get(direction, None)
+
+
+def update_wind_directions(data):
+    if data["nr_dir_vt"]:
+        degrees = wind_direction_to_degrees(data["nr_dir_vt"])
+        if degrees is not None:
+            data["nr_dir_vt"] = f"{degrees:000.1f}"
+        else:
+            data["nr_dir_vt"] = "N/A"  # ou algum valor padrão para direções desconhecidas
+    return data
+
 
 def save_to_csv(zone, station_name, data):
     if data:
+        # Atualize a direção do vento antes de salvar
+        data = update_wind_directions(data)
+
         file_path = f'clima_SP_zonas/{zone}/{station_name}.csv'
 
         data["Posto"] = station_name
@@ -168,27 +189,34 @@ def save_to_csv(zone, station_name, data):
 
         df = pd.DataFrame([data])
 
-        df.to_csv(file_path, mode='a', index=False, header=not os.path.exists(file_path),sep=';')
+        df.to_csv(file_path, mode='a', index=False, header=not os.path.exists(file_path), sep=';')
 
 
+def wait_until_next_hour():
+    now = datetime.now()
+    next_hour = now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
+    wait_time = (next_hour - now).total_seconds()
+    return wait_time
 
-import schedule
 
-def run_scraping():
+def wait_until_next_hour():
+    now = datetime.now()
+    next_hour = now.replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
+    wait_time = (next_hour - now).total_seconds()
+    return wait_time
+
+while True:
     for zone, stations in postos.items():
         for station_code, station_name in stations.items():
             station_url = BASE_URL.format(station_code)
             station_name_with_code = f"posto {station_code} - {station_name}"
 
             data = get_station_data(station_url)
+
             save_to_csv(zone, station_name_with_code, data)
 
-# Agende o script para rodar a cada hora cheia
-for hour in range(24):
-    schedule_time = f"{hour:02d}:00"
-    schedule.every().day.at(schedule_time).do(run_scraping)
+    # Calcule o tempo restante até a próxima hora cheia
+    sleep_time = wait_until_next_hour()
+    print(f"Aguardando {math.ceil(sleep_time)} segundos até o próximo horário.")
+    time.sleep(sleep_time)
 
-# Loop infinito para manter o agendamento ativo
-while True:
-    schedule.run_pending()
-    time.sleep(1)
